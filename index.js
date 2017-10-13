ddf776cab70d886ffa998f406825403730c6bdc3
Nico Kandut <nico.kandut@gmail.com>
2017-10-13 08:06:32 +0200

    now reads all files

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -9,10 +10,14 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.get('/', function (request, response) {
+    response.render('pages/index', { images: getAllImages() });
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+function getAllImages() {
+    return fs.readdirSync('./public');
+}
