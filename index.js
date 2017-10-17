e13f8593d8fabd954d7ca90a2dc97e38fed60d03
Pauli <pmuehlbacher13@gmail.com>
2017-10-17 13:20:53 +0200

    logogo.png

--- index.js
+++ index.js
@@ -13,33 +13,37 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function (request, response) {
-    response.render('pages/index', { images: getAllImages() });
+    response.render('pages/index', { images: getAllImages(function() {
+        return fs.readdirSync('./public').filter(filename => ['.png', '.jpg'].some(ext => filename.endsWith(ext)));
+    }) });
 });
 app.get('/display', function (request, response) {
     response.render('pages/display', { image: getImage(request.query.i) });
 });
-app.post('/display', function (request, response) {
+app.post('/display', function (req, res) {
     if  (!req.files)
         return  res.status(400).send('No files were uploaded.');
 
     // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-    let  sampleFile  =  req.files.sampleFile;
+    let  sampleFile  =  req.files.samplefile;
 
     // Use the mv() method to place the file somewhere on your server
-    sampleFile.mv('/public/' + req.files.sampleFile.name,  function (err)  {
+    sampleFile.mv('public/' + sampleFile.name,  function (err)  {
         if  (err)
             return  res.status(500).send(err);
-
-        res.send('File uploaded!');
     });
+    
+    res.render('pages/index', { images: getAllImages(function() {
+        return fs.readdirSync('./public').filter(filename => ['.png', '.jpg'].some(ext => filename.endsWith(ext)));
+    })});
 });
 
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
 });
 
-function getAllImages() {
-    return fs.readdirSync('./public').filter(filename => ['.png', '.jpg'].some(ext => filename.endsWith(ext)));
+function getAllImages(callback) {
+    return callback();
 }
 function getImage(imageName) {
     return {
