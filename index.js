96a4ba015a0b1dfe5a8f231391b38ed58652d4d7
Pauli <pmuehlbacher13@gmail.com>
2017-10-17 11:37:54 +0200

    paulis erste version

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get('/display', function (request, response) {
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 });
 
 function getAllImages() {
